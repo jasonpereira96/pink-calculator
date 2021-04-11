6e0fe5f4fb7b815cbc7d3b497afc0e673a2745a0
jasonpereira96 <jasonpereira074@gmail.com>
2021-04-11 22:08:11 +0530

    adding logic

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,13 +14,12 @@
                 </b-col>
               </b-row>
               <b-row>
-                <b-col cols="12" class="expression-row">
-                  34 + 35 + 400 + 20
+                <b-col cols="12" class="expression-row" v-html="formattedExpression">
                 </b-col>
               </b-row>
               <b-row>
                 <b-col cols="12" class="result-row">
-                  489
+                  {{ result }}
                 </b-col>
               </b-row>
             </b-container>
@@ -28,72 +27,72 @@
           <div class="controls">
             <b-container>
               <b-row>
-                <b-col cols="3" class="operator">
+                <b-col cols="3" class="operator" @click="onButtonClick(CLEAR)">
                   C
                 </b-col>
-                <b-col cols="3" class="operator">
+                <b-col cols="3" class="operator" @click="onButtonClick(PERC)">
                   %
                 </b-col>
-                <b-col cols="3" class="operator">
+                <b-col cols="3" class="operator" @click="onButtonClick(SQUARE_ROOT)">
                   &radic;
                 </b-col>
-                <b-col cols="3" class="operator">
+                <b-col cols="3" class="operator" @click="onButtonClick(DIVIDE)">
                   &divide;
                 </b-col>
               </b-row>
               <b-row>
-                <b-col cols="3" class="number">
+                <b-col cols="3" class="number" @click="onButtonClick(7)">
                   7
                 </b-col>
-                <b-col cols="3" class="number">
+                <b-col cols="3" class="number" @click="onButtonClick(8)">
                   8
                 </b-col>
-                <b-col cols="3" class="number">
+                <b-col cols="3" class="number" @click="onButtonClick(9)">
                   9
                 </b-col>
-                <b-col cols="3" class="operator">
+                <b-col cols="3" class="operator" @click="onButtonClick(MULTIPLY)">
                   &times;
                 </b-col>
               </b-row>
               <b-row>
-                <b-col cols="3" class="number">
+                <b-col cols="3" class="number" @click="onButtonClick(4)">
                   4
                 </b-col>
-                <b-col cols="3" class="number">
+                <b-col cols="3" class="number" @click="onButtonClick(5)">
                   5
                 </b-col>
-                <b-col cols="3" class="number">
+                <b-col cols="3" class="number" @click="onButtonClick(6)">
                   6
                 </b-col>
-                <b-col cols="3" class="operator">
+                <b-col cols="3" class="operator" @click="onButtonClick(MINUS)">
                   -
                 </b-col>
               </b-row>
               <b-row>
-                <b-col cols="3" class="number">
+                <b-col cols="3" class="number" @click="onButtonClick(1)">
                   1
                 </b-col>
-                <b-col cols="3" class="number">
+                <b-col cols="3" class="number" @click="onButtonClick(2)">
                   2
                 </b-col>
-                <b-col cols="3" class="number">
+                <b-col cols="3" class="number" @click="onButtonClick(3)">
                   3
                 </b-col>
-                <b-col cols="3" class="operator">
+                <b-col cols="3" class="operator" @click="onButtonClick(ADD)">
                   +
                 </b-col>
               </b-row>
               <b-row>
-                <b-col cols="3" class="number">
+                <b-col cols="3" class="number" @click="onButtonClick(0)">
                   0
                 </b-col>
-                <b-col cols="3" class="number">
+                <b-col cols="3" class="number" @click="onButtonClick(DOUBLE_ZERO)">
                   00
                 </b-col>
-                <b-col cols="3" class="operator">
+                <b-col cols="3" class="operator" @click="onButtonClick(DECIMAL)">
                   .
                 </b-col>
-                <b-col cols="3" class="equals">
+                <b-col cols="3" class="equals" @click="onEqualsClick()">
                   <div class="equals-wrapper">
                     =
                   </div>
@@ -108,8 +107,88 @@
 </template>
 
 <script>
+import constants from './../constants.js'
+
+function isNumber(item) {
+  return Number.isInteger(item)
+}
+function getLastChar(string) {
+  return string[string.length - 1]
+}
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      ...constants,
+      expression: '',
+      result: '0'
+    }
+  },
+  computed: {
+    formattedExpression() {
+      let { expression } = this
+      let result = expression
+      result = result.replaceAll('+', ' + ')
+      result = result.replaceAll('-', ' - ')
+      result = result.replaceAll('*', ' &times; ')
+      result = result.replaceAll('/', ' &divide; ')
+      // result = result.replaceAll('', '&radic;')
+      return result
+    }
+  },
+  methods: {
+    onEqualsClick() {
+      try {
+        let result = eval(this.expression)
+        this.result = result
+      } catch(e) {
+        this.result = 'Invalid Expression'
+      }
+    },
+    onButtonClick(item) {
+      if (item === 0) {
+        const lastChar = parseInt(getLastChar(this.expression))
+        if (isNumber(lastChar)) {
+          this.expression += '0'
+        }
+      } else if (isNumber(item)) {
+        this.expression += item
+      } else {
+        switch(item) {
+          case constants.ADD: {
+            this.expression += '+'
+          }
+          break
+          case constants.MINUS: {
+            this.expression += '-'
+          }
+          break
+          case constants.MULTIPLY: {
+            this.expression += '*'
+          }
+          break
+          case constants.DIVIDE: {
+            this.expression += '/'
+          }
+          break
+          case constants.DECIMAL: {
+            this.expression += '.'
+          }
+          break
+          case constants.CLEAR: {
+            this.expression = this.expression.slice(0, this.expression.length - 1)
+          }
+          break
+          case constants.DOUBLE_ZERO: {
+            const lastChar = parseInt(getLastChar(this.expression))
+            if (isNumber(lastChar)) {
+              this.expression += '00'
+            }
+          }
+        }
+      }
+    }
+  }
 }
 </script>
 
@@ -150,6 +229,7 @@ export default {
   cursor: pointer;
 }
 .equals-wrapper {
+  cursor: pointer;
   background: $pink;
   color: white;
   border-radius: 999px;
@@ -185,10 +265,13 @@ export default {
 }
 .expression-row {
   padding-top: 15px;
+  min-height: 24px;
 }
 .result-row {
   font-size: 2em;
   padding-bottom: 15px;
+  word-break: break-all;
+  min-height: 48px;
 }
 .main-container {
   display: flex;
---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const ADD = "ADD"
+export const MINUS = "MINUS"
+export const MULTIPLY = "MULTIPLY"
+export const DIVIDE = "DIVIDE"
+export const DEICMAL = "DEICMAL"
+export const DOUBLE_ZERO = "00"
+export const EQUALS = "EQUALS"
+export const CLEAR = "CLEAR"
+export const PERC = "PERC"
+export const SQUARE_ROOT = "SQUARE_ROOT"
+
+export default {
+    ADD, MINUS, MULTIPLY, DIVIDE, DEICMAL, DOUBLE_ZERO, EQUALS, PERC, SQUARE_ROOT, CLEAR
+}
